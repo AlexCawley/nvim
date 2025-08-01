e8d2cb7279d9e3916d24a5fbed361947dd91cade
Cawley, Alex <Alex.Cawley@lrs.com>
2025-08-01 15:34:42 -0500

    Add C# lsp and some more keymaps

--- init.lua
+++ init.lua
@@ -24,42 +24,95 @@ vim.opt.incsearch = true
 vim.opt.splitbelow = true
 vim.opt.splitright = true
 
-vim.keymap.set('n', "<leader>pv", vim.cmd.Ex)
+vim.keymap.set('n', '<leader>pv', ':Ex<cr>')
 
-vim.keymap.set('n', '<leader>j', ':split<CR>')
-vim.keymap.set('n', '<leader>l', ':vsplit<CR>')
+vim.keymap.set('n', '<leader>j', ':split<cr>')
+vim.keymap.set('n', '<leader>l', ':vsplit<cr>')
 
-vim.keymap.set('n', '<C-h>', '<C-w>h')
-vim.keymap.set('n', '<C-j>', '<C-w>j')
-vim.keymap.set('n', '<C-k>', '<C-w>k')
-vim.keymap.set('n', '<C-l>', '<C-w>l')
-
-vim.keymap.set('n', '<leader>ff', require('telescope.builtin').find_files)
-vim.keymap.set('n', '<leader>fg', require('telescope.builtin').live_grep)
-vim.keymap.set('n', '<leader>fb', require('telescope.builtin').buffers)
+vim.keymap.set('n', '<c-h>', '<c-w>h')
+vim.keymap.set('n', '<c-j>', '<c-w>j')
+vim.keymap.set('n', '<c-k>', '<c-w>k')
+vim.keymap.set('n', '<c-l>', '<c-w>l')
 
 vim.cmd [[packadd packer.nvim]]
 
 vim.cmd("colorscheme onedark")
 
-require('telescope').setup{
-  defaults = {
-    layout_strategy = 'horizontal',
-    layout_config = { prompt_position = "top" },
-    sorting_strategy = "ascending",
-    winblend = 0,
-  }
-}
-
 return require('packer').startup(function(use)
-  use 'wbthomason/packer.nvim'
+    use 'wbthomason/packer.nvim'
 
-  use "olimorris/onedarkpro.nvim"
+    use "olimorris/onedarkpro.nvim"
 
-  use {
-      'nvim-telescope/telescope.nvim',
-      tag = '0.1.5',
-      requires = { {'nvim-lua/plenary.nvim'} }
-  }
+    use {
+        'nvim-telescope/telescope.nvim',
+        tag = '0.1.5',
+        requires = { { 'nvim-lua/plenary.nvim' } },
+        config = function()
+            require('telescope').setup {
+                defaults = {
+                    layout_strategy = 'horizontal',
+                    layout_config = { prompt_position = "top" },
+                    sorting_strategy = "ascending",
+                    winblend = 0,
+                }
+            }
 
+            vim.keymap.set('n', '<leader>ff', require('telescope.builtin').find_files)
+            vim.keymap.set('n', '<leader>fg', require('telescope.builtin').live_grep)
+            vim.keymap.set('n', '<leader>fb', require('telescope.builtin').buffers)
+        end
+    }
+
+    use {
+        "williamboman/mason.nvim",
+        config = function()
+            require("mason").setup()
+        end
+    }
+
+    use {
+        "williamboman/mason-lspconfig.nvim",
+        config = function()
+            require("mason-lspconfig").setup {
+                ensure_installed = { "omnisharp" }
+            }
+        end
+    }
+
+    use {
+        "neovim/nvim-lspconfig",
+        config = function()
+            local lspconfig = require("lspconfig")
+
+            lspconfig.omnisharp.setup {
+                on_attach = function(client, bufnr)
+                    vim.keymap.set('n', 'gd', vim.lsp.buf.declaration)
+                    vim.keymap.set('n', 'gd', vim.lsp.buf.definition)
+                    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation)
+                    vim.keymap.set('n', 'gr', vim.lsp.buf.references)
+                    vim.keymap.set('n', 'k', vim.lsp.buf.hover)
+                    vim.keymap.set('n', '<c-k>', vim.lsp.buf.signarture_help)
+                    vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action)
+                    vim.keymap.set('v', '<leader>ca', vim.lsp.buf.range_code_action)
+                    vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float)
+                    vim.keymap.set('n', '<leader>f', function()
+                        vim.lsp.buf.format()
+                    end, bufopts)
+                end
+            }
+        end
+    }
+
+    use {
+        'ahmedkhalf/project.nvim',
+        config = function()
+            require("project_nvim").setup {
+                detection_methods = { "lsp", "pattern" },
+                patterns = { ".sln", ".csproj", ".git", "makefile" },
+            }
+            require('telescope').load_extension('projects')
+
+            vim.keymap.set('n', '<leader>fp', require('telescope').extensions.projects.projects)
+        end
+    }
 end)
