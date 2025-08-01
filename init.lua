cf3ac1c137aba0fa41ec7f225ab683239f9fd0e3
Cawley, Alex <Alex.Cawley@lrs.com>
2025-08-01 15:48:52 -0500

    Fix k command

--- init.lua
+++ init.lua
@@ -85,12 +85,13 @@ return require('packer').startup(function(use)
             local lspconfig = require("lspconfig")
 
             lspconfig.omnisharp.setup {
+                cmd = { vim.fn.stdpath("data") .. "/mason/packages/omnisharp/omnisharp" },
                 on_attach = function(client, bufnr)
                     vim.keymap.set('n', 'gd', vim.lsp.buf.declaration)
                     vim.keymap.set('n', 'gd', vim.lsp.buf.definition)
                     vim.keymap.set('n', 'gi', vim.lsp.buf.implementation)
                     vim.keymap.set('n', 'gr', vim.lsp.buf.references)
-                    vim.keymap.set('n', 'k', vim.lsp.buf.hover)
+                    vim.keymap.set('n', '<leader>k', vim.lsp.buf.hover)
                     vim.keymap.set('n', '<c-k>', vim.lsp.buf.signarture_help)
                     vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action)
                     vim.keymap.set('v', '<leader>ca', vim.lsp.buf.range_code_action)
