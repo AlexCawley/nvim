4e3927ae50fed669e3adcdb0af7f2f6ac8e3c293
Alex Cawley <alexcawley97@gmail.com>
2025-08-01 21:28:41 -0500

    Add completion

--- init.lua
+++ init.lua
@@ -1,4 +1,4 @@
-vim.g.mapleader = " "
+vim.g.mapleader = ' '
 
 vim.opt.number = true
 vim.opt.relativenumber = true
@@ -7,7 +7,7 @@ vim.opt.cursorline = true
 vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.undofile = false
-vim.opt.signcolumn = "yes"
+vim.opt.signcolumn = 'yes'
 vim.opt.scrolloff = 8
 
 vim.opt.expandtab = true
@@ -36,13 +36,29 @@ vim.keymap.set('n', '<C-l>', '<C-w>l')
 
 vim.cmd [[packadd packer.nvim]]
 
+local on_attach = function(client, bufnr)
+    local bufopts = { noremap=true, silent=true, buffer=bufnr }
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+    vim.keymap.set('n', '<leader>k', vim.lsp.buf.hover, bufopts)
+    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+    vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, bufopts)
+    vim.keymap.set('v', '<leader>ca', vim.lsp.buf.range_code_action, bufopts)
+    vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, bufopts)
+    vim.keymap.set('n', '<leader>f', function()
+        vim.lsp.buf.format()
+    end, bufopts)
+end
+
 return require('packer').startup(function(use)
     use 'wbthomason/packer.nvim'
 
     use { 
-        "olimorris/onedarkpro.nvim",
+        'olimorris/onedarkpro.nvim',
         config = function()
-            vim.cmd("colorscheme onedark")
+            vim.cmd('colorscheme onedark')
         end
     }
 
@@ -54,8 +70,8 @@ return require('packer').startup(function(use)
             require('telescope').setup {
                 defaults = {
                     layout_strategy = 'horizontal',
-                    layout_config = { prompt_position = "top" },
-                    sorting_strategy = "ascending",
+                    layout_config = { prompt_position = 'top' },
+                    sorting_strategy = 'ascending',
                     winblend = 0,
                 }
             }
@@ -67,38 +83,78 @@ return require('packer').startup(function(use)
     }
 
     use {
-        "williamboman/mason.nvim",
+        'hrsh7th/nvim-cmp',
+        requires = {
+            'hrsh7th/cmp-nvim-lsp',
+            'hrsh7th/cmp-buffer',   
+            'hrsh7th/cmp-path',      
+            'L3MON4D3/LuaSnip',       
+            'saadparwaiz1/cmp_luasnip' 
+        },
         config = function()
-            require("mason").setup()
+            local cmp = require('cmp')
+            local luasnip = require('luasnip')
+        
+            cmp.setup {
+            snippet = {
+                expand = function(args)
+                luasnip.lsp_expand(args.body)
+                end,
+            },
+            mapping = cmp.mapping.preset.insert({
+                ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+                ['<C-f>'] = cmp.mapping.scroll_docs(4),
+                ['<C-Space>'] = cmp.mapping.complete(),
+                ['<C-e>'] = cmp.mapping.abort(),
+                ['<CR>'] = cmp.mapping.confirm({ select = true }),
+            }),
+            sources = cmp.config.sources({
+                { name = 'nvim_lsp' },
+                { name = 'luasnip' },
+            }, {
+                { name = 'buffer' },
+            })
+        }
+    end
+    }
+
+    use {
+        'williamboman/mason.nvim',
+        config = function()
+            require('mason').setup()
         end
     }
 
     use {
-        "williamboman/mason-lspconfig.nvim",
+        'williamboman/mason-lspconfig.nvim',
         config = function()
-            require("mason-lspconfig").setup {
-                ensure_installed = { "omnisharp" }
+            require('mason-lspconfig').setup {
+                ensure_installed = { 
+                    'omnisharp'
+                }
             }
         end
     }
 
     use {
-        "neovim/nvim-lspconfig",
+        'neovim/nvim-lspconfig',
         config = function()
-            local lspconfig = require("lspconfig")
+            local lspconfig = require('lspconfig')
 
             lspconfig.omnisharp.setup {
-                cmd = { vim.fn.stdpath("data") .. "/mason/packages/omnisharp/omnisharp" },
+                capabilities = require('cmp_nvim_lsp').default_capabilities(),
+                cmd = { vim.fn.stdpath('data') .. '/mason/packages/omnisharp/omnisharp' },
                 on_attach = function(client, bufnr)
-                    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration)
-                    vim.keymap.set('n', 'gd', vim.lsp.buf.definition)
-                    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation)
-                    vim.keymap.set('n', 'gr', vim.lsp.buf.references)
-                    vim.keymap.set('n', '<leader>k', vim.lsp.buf.hover)
-                    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help)
-                    vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action)
-                    vim.keymap.set('v', '<leader>ca', vim.lsp.buf.range_code_action)
-                    vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float)
+                    local bufopts = { noremap=true, silent=true, buffer=bufnr }
+                    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+                    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+                    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+                    vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+                    vim.keymap.set('n', '<leader>k', vim.lsp.buf.hover, bufopts)
+                    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+                    vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, bufopts)
+                    vim.keymap.set('v', '<leader>ca', vim.lsp.buf.range_code_action, bufopts)
+                    vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, bufopts)
                     vim.keymap.set('n', '<leader>f', function()
                         vim.lsp.buf.format()
                     end, bufopts)
@@ -110,9 +166,9 @@ return require('packer').startup(function(use)
     use {
         'ahmedkhalf/project.nvim',
         config = function()
-            require("project_nvim").setup {
-                detection_methods = { "lsp", "pattern" },
-                patterns = { ".sln", ".csproj", ".git", "makefile" },
+            require('project_nvim').setup {
+                detection_methods = { 'lsp', 'pattern' },
+                patterns = { '.sln', '.csproj', '.git', 'makefile' },
             }
             require('telescope').load_extension('projects')
 
