fa40e12e546e1a255715553ec63d438638dea23f
Cawley, Alex <Alex.Cawley@lrs.com>
2025-09-04 16:00:39 -0500

    Simplify config

--- init.lua
+++ init.lua
@@ -34,12 +34,15 @@ vim.keymap.set('n', '<C-j>', '<C-w>j')
 vim.keymap.set('n', '<C-k>', '<C-w>k')
 vim.keymap.set('n', '<C-l>', '<C-w>l')
 
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
     use 'wbthomason/packer.nvim'
 
-    use { 
+    use {
         'olimorris/onedarkpro.nvim',
         config = function()
             vim.cmd('colorscheme onedark')
@@ -67,91 +70,14 @@ return require('packer').startup(function(use)
     }
 
     use {
-        'hrsh7th/nvim-cmp',
-        requires = {
-            'hrsh7th/cmp-nvim-lsp',
-            'hrsh7th/cmp-buffer',   
-            'hrsh7th/cmp-path',      
-            'L3MON4D3/LuaSnip',       
-            'saadparwaiz1/cmp_luasnip' 
-        },
+        'nvim-tree/nvim-tree.lua',
+        requires = { 'nvim-tree/nvim-web-devicons' },
         config = function()
-            local cmp = require('cmp')
-            local luasnip = require('luasnip')
-        
-            cmp.setup {
-            snippet = {
-                expand = function(args)
-                luasnip.lsp_expand(args.body)
-                end,
-            },
-            mapping = cmp.mapping.preset.insert({
-                ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-                ['<C-f>'] = cmp.mapping.scroll_docs(4),
-                ['<C-Space>'] = cmp.mapping.complete(),
-                ['<C-e>'] = cmp.mapping.abort(),
-                ['<CR>'] = cmp.mapping.confirm({ select = true }),
-            }),
-            sources = cmp.config.sources({
-                { name = 'nvim_lsp' },
-                { name = 'luasnip' },
-            }, {
-                { name = 'buffer' },
-            })
-        }
-    end
-    }
-
-    use {
-        'williamboman/mason.nvim',
-        config = function()
-            require('mason').setup()
-        end
-    }
-
-    use {
-        'williamboman/mason-lspconfig.nvim',
-        config = function()
-            require('mason-lspconfig').setup {
-                ensure_installed = { 
-                    'omnisharp'
+            require('nvim-tree').setup {
+                view = {
+                    width = 30
                 }
             }
-        end
-    }
-
-    use {
-        'neovim/nvim-lspconfig',
-        config = function()
-            local lspconfig = require('lspconfig')
-
-            lspconfig.omnisharp.setup {
-                capabilities = require('cmp_nvim_lsp').default_capabilities(),
-                cmd = { vim.fn.stdpath('data') .. '/mason/packages/omnisharp/omnisharp' },
-                on_attach = function(client, bufnr)
-                    local bufopts = { noremap=true, silent=true, buffer=bufnr }
-                    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
-                    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
-                    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
-                    vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
-                    vim.keymap.set('n', '<leader>f', function()
-                        vim.lsp.buf.format()
-                    end, bufopts)
-                end
-            }
-        end
-    }
-
-    use {
-        'ahmedkhalf/project.nvim',
-        config = function()
-            require('project_nvim').setup {
-                detection_methods = { 'lsp', 'pattern' },
-                patterns = { '.sln', '.csproj', '.git', 'makefile' },
-            }
-            require('telescope').load_extension('projects')
-
-            vim.keymap.set('n', '<leader>fp', require('telescope').extensions.projects.projects)
         end
     }
 end)
